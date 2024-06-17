7efc4f195e9a8fbd4a0fd5f4e069d802f2d73d66
ChlowWenyiZhang <51265903051@stu.ecnu.edu.cn>
2024-06-17 16:26:35 +0800

    generate graph

--- demo.py
+++ demo.py
@@ -11,39 +11,34 @@ npm_dependencies_df = pd.read_csv('/OpenSource/npm_data/2024-05-13-17-23-22_EXPO
 print(npm_packages_df.shape)
 print(npm_dependencies_df.shape)
 
+# 构建一个包id到名称的映射字典
+package_id_to_name = pd.Series(npm_packages_df.name.values, index=npm_packages_df.package_id).to_dict()
+
 # 创建一个有向图
 G = nx.DiGraph()
 
-# 添加节点
-for idx, row in npm_packages_df.iterrows():
-    G.add_node(row['package_id'],
-               name=row['name'],
-               version=row['version'],
-               description=row['description'],
-               repository_type=row['repository_type'],
-               repository_url=row['repository_url'],
-               license=row['license'],
-               homepage=row['homepage'],
-               time=row['time'])
-
-print("Add nodes over.")
+for package_id, package_name in package_id_to_name.items():
+    G.add_node(package_name)
 
 # 添加边（依赖关系）
-i = 0
-for idx, row in npm_dependencies_df.iterrows():
-    G.add_edge(row['package_id'], row['dependency_name'])
-    if i % 10000 == 0:
-        print("i")
-        i += 1
+for index, row in npm_dependencies_df.iterrows():
+    package_id = row['package_id']
+    dependency_name = row['dependency_name']
+
+    if package_id in package_id_to_name.keys():
+        package_name = package_id_to_name[package_id]
+        if dependency_name in package_id_to_name.values():
+            G.add_edge(package_name, dependency_name)
 
 print("Add edges over.")
 
-# 可视化依赖网络
-plt.figure()
-pos = nx.spring_layout(G, k=0.1)
-nx.draw(G, pos, with_labels=True, node_size=1, font_size=8, edge_color='gray', alpha=0.7)
-plt.savefig('./network.png')
-plt.close()
+# # 可视化依赖网络
+# plt.figure(figsize=(12, 12))
+# pos = nx.spring_layout(G, k=0.1)
+# nx.draw(G, pos, with_labels=True, node_size=1, font_size=8, node_color='lightblue', edge_color='gray', arrows=True, alpha=0.8)
+# plt.title('npm Dependency Network')
+# plt.savefig('./network.png')
+# plt.close()
 
 # 分析网络
 degree_centrality = nx.degree_centrality(G)
